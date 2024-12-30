90874d05626bd12158c1f9419a39531d226558d6
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-12-30 12:01:46 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+// +++++++++++ W3 ++++++++++++++++++++++
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Hello(Props) {
+  return <h1>Hellow React.js</h1>;
+}
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<Hello />);
