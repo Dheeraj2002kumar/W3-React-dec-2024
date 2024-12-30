9a3d6f837e08227779797d39079b9ad36b093e26
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-12-30 10:00:15 +0000

    class inheritance

--- src/test_js/ES6.js
+++ src/test_js/ES6.js
@@ -1,3 +1,5 @@
+/*
+
 // Create an object called 'mycar' based on the Car class:
 
 class Car {
@@ -8,8 +10,10 @@ class Car {
 const mycar = new Car("Ford");
 // console.log(`This is my car ${mycar.brand}.`);
 
-// ----------- Method in classes ----------
+*/
 
+// ----------- Method in classes ----------
+/*
 class Car1 {
   constructor(name) {
     this.brand = name;
@@ -22,3 +26,37 @@ class Car1 {
 
 const myCar = new Car1("Ford");
 // console.log(myCar.present());
+*/
+
+// -------------- Class Inheritance -------------------
+// To create a class inheritance, use the extends keyword.
+
+// A class created with a class inheritance inherits all the methods from another class:
+
+class Car {
+  constructor(name) {
+    this.brand = name;
+  }
+
+  present() {
+    return "I have a " + this.brand;
+  }
+}
+
+class Model extends Car {
+  constructor(name, mod) {
+    super(name);
+    this.model = mod;
+  }
+
+  show() {
+    return this.present() + ", it is a " + this.model;
+  }
+}
+
+const myCar = new Model("Ford", "Mustang");
+// console.log(myCar.show());
+
+// The super() method refers to the parent class.
+
+// By calling the super() method in the constructor method, we call the parent's constructor method and get access to the parent's properties and methods.
