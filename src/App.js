9ab426afad96f41d087afee85e5de367743e9499
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-12-31 07:09:04 +0000

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./styles.css";
 
+// ------------------------------
+
+/* 
+Attribute class = className
+The class attribute is a much used attribute in HTML, but since JSX is rendered as JavaScript, and the class keyword is a reserved word in JavaScript, you are not allowed to use it in JSX.
+
+Use attribute className instead.
+
+JSX solved this by using className instead. When JSX is rendered, it translates className attributes into class attributes.
+*/
+
+const myElement5 = <h1 className="myElement5">Hello React</h1>;
+
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!{myList}</h2>
-    </div>
-  );
+  return myElement5;
 }
