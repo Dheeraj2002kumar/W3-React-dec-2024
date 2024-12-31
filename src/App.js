4e509672ce2874c665c781efd0d1e5cb625a8de9
Dheeraj Kumar <114610396+Dheeraj2002kumar@users.noreply.github.com>
2024-12-31 07:19:33 +0000

    React JSX

--- src/App.js
+++ src/App.js
@@ -13,6 +13,49 @@ JSX solved this by using className instead. When JSX is rendered, it translates 
 
 const myElement5 = <h1 className="myElement5">Hello React</h1>;
 
+// -------------------------------
+
+// +++++++++++++ Conditions - if statements ++++++
+
+/* 
+React supports if statements, but not inside JSX.
+
+To be able to use conditional statements in JSX, you should put the if statements outside of the JSX, or you could use a ternary expression instead:
+
+Option 1:
+Write if statements outside of the JSX code:
+
+Example
+Write "Hello" if x is less than 10, otherwise "Goodbye":
+*/
+
+const x = 5;
+let text = "Goodbye";
+if (x < 10) {
+  text = "Hello!";
+}
+
+const myElement6 = <h1>{text}</h1>;
+
+// ----------------------
+
+/**
+ Option 2:
+Use ternary expressions instead:
+
+Example
+Write "Hello" if x is less than 10, otherwise "Goodbye":
+
+ */
+
+const x1 = 5;
+
+const myElement7 = <h1>{x1 < 10 ? "Hello React!" : "Goodbye"}</h1>;
+
+// Note that in order to embed a JavaScript expression inside JSX, the JavaScript must be wrapped with curly braces, {}.
+
+// -------------------------------
+
 export default function App() {
-  return myElement5;
+  return myElement7;
 }
